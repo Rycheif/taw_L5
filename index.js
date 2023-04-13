c12d584d52e08082e395d689de906392226039a1
Michal <mbaka0002@gmail.com>
2023-04-13 19:19:54 +0200

    Lab 6

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const express = require('express');
-const questions = require('./questions').questions;
-const config = require('./config').config;
-
-const app = express();
-
-app.get('/api/questions', (req, res) => {
-    res.send(questions);
-});
-
-app.listen(config.port, () => {
-    console.info(`Server is running on port 3000`);
-});
+require( 'babel-core/register' );
+require( './app/app');
