f3056c993e567130507fdf72b1911aa0a6185e2a
Michal <mbaka0002@gmail.com>
2023-04-29 11:01:17 +0200

    Lab 7

---
+++ app/DAO/passwordDAO.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import * as _ from 'lodash';
+import mongoConverter from "../service/mongoConverter";
+import applicationException from "../service/applicationException";
+
+const passwordSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true, unique: true},
+    password: {type: String, required: true}
+}, {
+    collection: 'password'
+});
+
+const PasswordModel = mongoose.model('password', passwordSchema);
+
+async function createOrUpdate(data) {
+    const result = await PasswordModel.findOneAndUpdate({userId: data.userId},
+        _.omit(data, 'id'), {new: true});
+    if (!result) {
+        return __createPasswordModel(data);
+    }
+    return result;
+}
+
+async function __createPasswordModel(data) {
+    const result = await new PasswordModel({
+        userId: data.userId,
+        password: data.password
+    }).save();
+    if (result) {
+        return mongoConverter(result);
+    }
+    return result;
+}
+
+async function authorize(userId, password) {
+    const result = await PasswordModel.findOne({userId: userId, password: password});
+    if (result && mongoConverter(result)) {
+        return true;
+    }
+    throw applicationException.new(applicationException.UNAUTHORIZED,
+        'User and password does not match');
+}
+
+export default {
+    createOrUpdate: createOrUpdate,
+    authorize: authorize,
+
+    model: PasswordModel
+};
+
